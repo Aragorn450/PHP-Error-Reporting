ab365bdb34791ac084689063e3ddb5ee1fb28c3e
Charles Lehardy <charlie.lehardy@gmail.com>
2013-10-06 16:40:34 -0700

    Update ErrorReporting.php
    Updated the error handler to catch E_ERROR errors as well.

--- ErrorReporting.php
+++ ErrorReporting.php
@@ -1,9 +1,16 @@
 <?php
-# JM Namespace
-namespace JM;
 
-# Set the default error handler for php to this class
-set_error_handler('JM\ErrorHandler::handle');
+/**
+ * PHP Custom Error Handler
+ * 
+ * @package ErrorHandler
+ * @author James McFall <james@mcfall.geek.nz>
+ * @author Charlie Lehardy <charlie.lehardy@gmail.com>
+ */
+
+// Set the default error handler for php to this class
+set_error_handler('ErrorHandler::handleError');
+register_shutdown_function('ErrorHandler::shutdownHandler');
 
 # @todo - Don't forget to set error numb threashold
 # @todo - Log to screen in certain environments. Make so Bootstrapper can set up.
@@ -14,9 +21,9 @@ set_error_handler('JM\ErrorHandler::handle');
  * This class can be used to overwrite the default error handling behaviour 
  * of php and provide a more comprehensive error message 
  * 
- * @version 0.1
+ * @version 0.2
  * @author James McFall <james@mcfall.geek.nz>
- * @date 30 Aug 2012
+ * @author Charlie Lehardy <charlie.lehardy@gmail.com>
  */
 class ErrorHandler {
     
@@ -26,28 +33,25 @@ class ErrorHandler {
     public static $logViaScreen = true;
     
     # Error logging timezone
-    public static $errorLogTimezone = "Pacific/Auckland";
+    public static $errorLogTimezone = "America/Phoenix";
     
     # Set who the error emails should be sent to
-    public static $emailTo = "james@mcfall.geek.nz";
+    public static $emailTo = "charlie.lehardy@gmail.com";
     
     # Set error log file location 
-    public static $logFileLocation = "/Users/james/Projects/Playground/logs/errors.log";
-    
-    
+    public static $logFileLocation = "/tmp/php_error_handler/errors.log";
     
     /**
      * This method is the main method that handles all of the functionality for
      * the handler.
      * 
-     * @param <int> $errorCode - Level of the error raised.
-     * @param <string> $message - Error message.
-     * @param <string> $file - File of origin the error occurred in.
-     * @param <int> $lineNumber - Line number the error occured on in the file of origin.
-     * @param <array> $vars - A large array of all variables that exist in scope at the time of the error.
+     * @param int $errorCode - Level of the error raised.
+     * @param string $message - Error message.
+     * @param string $file - File of origin the error occurred in.
+     * @param int $lineNumber - Line number the error occured on in the file of origin.
+     * @param array $vars - A large array of all variables that exist in scope at the time of the error.
      */
-    public static function handle ($errorCode, $message, $file, $lineNumber, $vars) {
-        
+    public static function handleError($errorCode, $message, $file, $lineNumber, $vars = new Array()) {
         # Send out email error notification.
         if (self::$logViaEmail)
             self::sendErrorEmail($errorCode, $message, $file, $lineNumber, $vars);
@@ -57,11 +61,25 @@ class ErrorHandler {
             self::writeToErrorLog($errorCode, $message, $file, $lineNumber, $vars);
         
         # Log the error to the screen
-        if (self::$logViaScreen)
+        if (self::$logViaScreen) {
             self::logErrorToScreen($errorCode, $message, $file, $lineNumber, $vars);
-        
+        }
     }
     
+    /**
+     * This method catches a shutdown of PHP and if it's because an E_ERROR was thrown, it
+     * will send the message to the error handler.
+     * 
+     * @uses ErrorHandler::handleError()
+     */
+    public static function handleShutdown() {
+        $last_error = error_get_last();
+        
+        // Check if the last error is a fatal error.
+        if ($last_error['type'] === E_ERROR) {
+            self::handleError(E_ERROR, $last_error['message'], $last_error['file'], $last_error['line']);
+        }
+    }
     
     /**
      * This function can be used to overwrite the default error handling behaviour 
@@ -70,17 +88,15 @@ class ErrorHandler {
      * This gives a large amount of data around the error as it can easily be fit
      * into an email message.
      * 
-     * @param <int> $errorCode - Level of the error raised.
-     * @param <string> $message - Error message.
-     * @param <string> $file - File of origin the error occurred in.
-     * @param <int> $lineNumber - Line number the error occured on in the file of origin.
-     * @param <array> $vars - A large array of all variables that exist in scope at the time of the error.
+     * @param int $errorCode - Level of the error raised.
+     * @param string $message - Error message.
+     * @param string $file - File of origin the error occurred in.
+     * @param int $lineNumber - Line number the error occured on in the file of origin.
+     * @param array $vars - A large array of all variables that exist in scope at the time of the error.
      */
     static function sendErrorEmail($errorCode, $message, $file, $lineNumber, $vars) {
-
-        $timezone = new \DateTimeZone(self::$errorLogTimezone);
-        $time = new \DateTime("now", $timezone);
-        
+        $timezone = new DateTimeZone(self::$errorLogTimezone);
+        $time = new DateTime("now", $timezone);
         
         # Start building an error message
         $errorMessage = "<p><h2>An unhandled error occurred on http://" . $_SERVER['HTTP_HOST'] . "</h2></p>";
@@ -113,16 +129,15 @@ class ErrorHandler {
     /**
      * This method writes the error message to the log file if it's available.
      * 
-     * @param <int> $errorCode - Level of the error raised.
-     * @param <string> $message - Error message.
-     * @param <string> $file - File of origin the error occurred in.
-     * @param <int> $lineNumber - Line number the error occured on in the file of origin.
-     * @param <array> $vars - A large array of all variables that exist in scope at the time of the error.
+     * @param int $errorCode - Level of the error raised.
+     * @param string $message - Error message.
+     * @param string $file - File of origin the error occurred in.
+     * @param int $lineNumber - Line number the error occured on in the file of origin.
+     * @param array $vars - A large array of all variables that exist in scope at the time of the error.
      */
     public static function writeToErrorLog($errorCode, $message, $file, $lineNumber, $vars) {
-        
-        $timezone = new \DateTimeZone(self::$errorLogTimezone);
-        $time = new \DateTime("now", $timezone);
+        $timezone = new DateTimeZone(self::$errorLogTimezone);
+        $time = new DateTime("now", $timezone);
         
         # Start building an error message
         $errorMessage = $time->format('h:i:sa d M Y') .": An unhandled error($errorCode) ";
@@ -138,16 +153,15 @@ class ErrorHandler {
     
     /**
      *
-     * @param <int> $number - Level of the error raised.
-     * @param <string> $message - Error message.
-     * @param <string> $file - File of origin the error occurred in.
-     * @param <int> $lineNumber - Line number the error occured on in the file of origin.
-     * @param <array> $vars - A large array of all variables that exist in scope at the time of the error.
+     * @param int $number - Level of the error raised.
+     * @param string $message - Error message.
+     * @param string $file - File of origin the error occurred in.
+     * @param int $lineNumber - Line number the error occured on in the file of origin.
+     * @param array $vars - A large array of all variables that exist in scope at the time of the error.
      */
     public static function logErrorToScreen($number, $message, $file, $lineNumber, $vars) {
-        $timezone = new \DateTimeZone(self::$errorLogTimezone);
-        $time = new \DateTime("now", $timezone);
-        
+        $timezone = new DateTimeZone(self::$errorLogTimezone);
+        $time = new DateTime("now", $timezone);
         
         # Start building an error message
         $errorMessage = "<p><h2>An unhandled error occurred on http://" . $_SERVER['HTTP_HOST'] . "</h2></p>";
@@ -174,12 +188,6 @@ class ErrorHandler {
         echo $errorMessage;
         
         exit();
-        
     }
     
 }
-
-# Cause error by trying to output a variable that doesn't exist and looks like a fart noise
-echo $bbrrrrrt;
-
-?>
